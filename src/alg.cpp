b023f95b2c11fb757ecfc2383450895f54129bc0
Vladislav Grachev <99138298+Vladislav-Grachev@users.noreply.github.com>
2022-02-27 17:50:49 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,17 +4,53 @@
 
 
 bool checkPrime(uint64_t value) {
-// вставьте код функции
+	uint64_t x = 2;
+	while (x * x <= value)
+	{
+		if (value % x != 1)
+			return false;
+		else
+			++x;
+	}
+	return true;
 }
-
 uint64_t nPrime(uint64_t n) {
-// вставьте код функции
-}
+	uint64_t x = 1, value{};
+	for (int a = 1;
+			a <= n;
+			a++)
+	{
+		while (true)
+		{
+			x++;
+			value = checkPrime(x);
+			if (value) break;
+		}
 
-uint64_t nextPrime(uint64_t value) {
-// вставьте код функции
-}
+	}
+	return x;
 
+}
+uint64_t nextPrime(uint64_t value) {{
+	uint64_t k = 1;
+	while (!checkPrime(value + k)) {
+		k++;
+	}
+	return value + k;
+}
 uint64_t sumPrime(uint64_t hbound) {
-// вставьте код функции
+	uint64_t sum = 0;
+	for (int k = 2; k < hbound;
+       k++) {
+		if (checkPrime(k)) 
+      sum += k;
+	}
+	return sum;
 }
+int main()
+{
+	printf(" % d", checkPrime(13));
+	printf(" % I64d", nPrime(99));
+	printf(" % I64d", nextPrime(7));
+	printf(" % I64d", sumPrime(8));
+}                                  
